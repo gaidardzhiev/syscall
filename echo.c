db7ecc17703419348d9df567c6203311627dc0fc
gaidardzhiev <1v4n@protonmail.com>
2025-04-19 07:44:28 +0000

    Sat Apr 19 07:44:27 AM UTC 2025

---
+++ echo.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <string.h>
+
+void f(const char *e) {
+	size_t n = strlen(e);
+	int d = STDOUT_FILENO;
+	const char *m = e;
+	size_t c = n;
+	int l = 4;
+	asm volatile(
+		"mov r7, %[o]\n\t"
+		"mov r0, %[d]\n\t"
+		"mov r1, %[m]\n\t"
+		"mov r2, %[c]\n\t"
+		"svc #0"
+		:
+		: [d] "r"(d), [m] "r"(m), [c] "r"(c), [o] "r"(l)
+		: "r0", "r1", "r2", "r7", "memory"
+	);
+}
+
+int main(int z, char **x) {
+	if(z < 2) return 1;
+	for(int i = 1; i < z; i++) {
+		f(x[i]);
+		if(i < z-1) f(" ");
+	}
+	f("\n");
+	return 0;
+}

4247a858e6a7737ac2479a6f3ea7b6d3ca60a089
gaidardzhiev <1v4n@protonmail.com>
2025-04-20 08:40:42 +0000

    Sun Apr 20 08:40:42 AM UTC 2025

--- test.sh
+++ test.sh
@@ -10,6 +10,23 @@ ffalse() {
 	}
 }
 
-{ ffalse; RET=$?; } || exit 1
+fsleep() {
+	s=$(date +%s)
+	./sleep 1 && {
+		e=$(date +%s);
+		t=$((e - s));
+		[ "$t" -eq 1 ];
+	} && {
+		printf "./sleep PASSED...\n";
+		return 0;
+	} || {
+		printf "./sleep FAILED...\n";
+		return 3;
+	}
+}
+
+
+
+{ ffalse && fsleep; RET=$?; } || exit 1
 
 [ "$RET" -ne 0 ] && printf "%d\n" "$RET"

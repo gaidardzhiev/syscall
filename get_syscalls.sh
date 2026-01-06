6df1b2f640339322150f118fdd618be3f37a3bea
gaidardzhiev <1v4n@protonmail.com>
2026-01-06 09:10:10 +0000

    Tue Jan  6 09:10:10 AM UTC 2026

--- get_syscalls.sh
+++ get_syscalls.sh
@@ -14,7 +14,7 @@ command -v cpp > /dev/null || {
 
 cpp -include sys/syscall.h -dM </dev/null \
        	| sed -n "s/^#define ${p}//p" \
-       	| sort -k1 && exit 0 || {
+	| sort -k1 && exit 0 || {
 		printf "something's wrong in here somewhere...\n" >&2;
 		exit 1;
 }

9de790b79fb352fb1f000a3c3029a133461bc5a3
gaidardzhiev <1v4n@protonmail.com>
2026-01-07 09:29:37 +0000

    rely on start.S like crt0 so yes gets sane argc/argv in _ep

--- yes.c
+++ yes.c
@@ -22,15 +22,40 @@ void fs(const char *s) {
 }
 
 void _ep() {
-	register int p asm("r0");
-	register char **q asm("r1");
-	asm volatile ("" : "=r" (p), "=r" (q) : : );
+	int p;
+	char **q;
+	asm volatile (
+		"mov %0, r0\n\t"
+		"mov %1, r1\n\t"
+		: "=r"(p), "=r"(q)
+		:
+		: "memory"
+	);
+	char b[32];
+	int n = p;
+	int i = 0;
+	if (n == 0) {
+		b[i++] = '0';
+	} else {
+		char t[16];
+		int j = 0;
+		while (n > 0 && j < 16) {
+			t[j++] = '0' + (n % 10);
+			n /= 10;
+		}
+		while (j > 0) {
+			b[i++] = t[--j];
+		}
+	}
+	b[i++] = '\n';
+	b[i] = '\0';
+	fs(b);
 	const char *m = "y";
 	if (p > 1) {
 		m = (const char *)q[1];
 	}
 	char nl = '\n';
-	while (1) {
+	for (;;) {
 		fs(m);
 		fw(4, (void*)1, &nl, 1);
 	}
